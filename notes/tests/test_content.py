5260f9a6045dbbbc8a876919382dd00044c8a8b2
Максим Давыдов <m.davidov92@gmail.com>
2023-11-26 19:53:30 +0300

    Add test content

--- notes/tests/test_content.py
+++ notes/tests/test_content.py
@@ -1,40 +1,68 @@
-# from django.conf import settings
-# from django.contrib.auth import get_user_model
-# from django.test import TestCase
-# from django.urls import reverse
-# from pytils.translit import slugify
+from django.conf import settings
+from django.contrib.auth import get_user_model
+from django.test import Client, TestCase
+from django.urls import reverse
 
-# from notes.models import Note
+from notes.models import Note
 
-# User = get_user_model()
-
-
-# class TestDetailPage(TestCase):
-
-#     @classmethod
-#     def setUpTestData(cls):
-#         cls.note = Note.objects.create(
-#             title='Тестовая новость',
-#             text='Просто текст.',
-#             slug=slugify('title'),
-#         )
-#         cls.detail_url = reverse('notes:detail', args=(cls.note.slug,))
-#         cls.author = User.objects.create(username='Комментатор')
-#         for index in range(2):
-#             note = Note.objects.create(
-#                 news=cls.note, author=cls.author, text=f'Tекст {index}',
-#             )
-#             note.save()
-
-#     def test_notes_order(self):
-#         response = self.client.get(self.detail_url)
-#         self.assertIn('note', response.context)
-#         news = response.context['note']
-#         all_notes = news.note_set.all()
-#         self.assertLess(all_notes[0].created, all_notes[1].created)
+User = get_user_model()
 
 
-#     def test_authorized_client_has_form(self):
-#         self.client.force_login(self.author)
-#         response = self.client.get(self.detail_url)
-#         self.assertIn('form', response.context)
+class TestContent(TestCase):
+    LIST_URL = reverse('notes:list')
+
+    @classmethod
+    def setUpTestData(cls):
+        author = User.objects.create(username='Автор')
+        cls.author = Client()
+        cls.author.force_login(author)
+        reader = User.objects.create(username='Читатель')
+        cls.reader = Client()
+        cls.reader.force_login(reader)
+        all_notes = [
+            Note(
+                title=f'Заголовок {index}',
+                text='Текст заметки',
+                slug=f'{index}',
+                author=author,
+            )
+            for index in range(settings.NOTE_COUNT_ON_LIST_PAGE)
+        ]
+        cls.note = Note.objects.bulk_create(all_notes)
+
+    def test_note_not_in_list_for_another_user(self):
+        response = self.author.get(self.LIST_URL)
+        object_list = response.context['object_list']
+        notes_count = len(object_list)
+        self.assertEqual(notes_count, settings.NOTE_COUNT_ON_LIST_PAGE)
+
+    def test_notes_list_for_different_users(self):
+        response = self.reader.get(self.LIST_URL)
+        object_list = response.context['object_list']
+        notes_count = len(object_list)
+        self.assertNotEqual(notes_count, settings.NOTE_COUNT_ON_LIST_PAGE)
+
+
+class TestFormPage(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        author = User.objects.create(username='Автор')
+        cls.author = Client()
+        cls.author.force_login(author)
+        cls.note = Note.objects.create(
+            title='Заголовок',
+            text='Текст заметки',
+            slug='not-slug',
+            author=author
+        )
+
+    def test_create_note_page_contains_form(self):
+        url = reverse('notes:add')
+        response = self.author.get(url)
+        self.assertIn('form', response.context)
+
+    def test_edit_note_page_contains_form(self):
+        edit_url = reverse('notes:edit', args=(self.note.slug,))
+        response = self.author.get(edit_url)
+        self.assertIn('form', response.context)
--- notes/tests/test_logic.py
+++ notes/tests/test_logic.py
@@ -1,42 +1,82 @@
-from http import HTTPStatus
+# from http import HTTPStatus
 
-from django.contrib.auth import get_user_model
-from django.test import Client, TestCase
-from django.urls import reverse
+# from django.contrib.auth import get_user_model
+# from django.test import Client, TestCase
+# from django.urls import reverse
 
-from notes.forms import WARNING
-from notes.models import Note
+# from notes.forms import WARNING
+# from notes.models import Note
 
-User = get_user_model()
+# User = get_user_model()
 
 
-class TestNoteCreation(TestCase):
-    NOTE_TEXT = 'Текст комментария'
+# class TestNoteCreation(TestCase):
+#     NOTE_TEXT = 'Текст комментария'
 
-    @classmethod
-    def setUpTestData(cls):
-        cls.note = Note.objects.create(
-            title='Заголовок',
-            text='Текст',
-            slug='not-slug',
-        )
-        cls.url = reverse('notes:detail', args=(cls.note.slug,))
-        cls.user = User.objects.create(username='Автор')
-        cls.auth_client = Client()
-        cls.auth_client.force_login(cls.user)
-        cls.form_data = {'text': cls.NOTE_TEXT}
+#     @classmethod
+#     def setUpTestData(cls):
+#         cls.notes = Note.objects.create(
+#             title='Заголовок',
+#             text='Текст',
+#             slug='not-slug',
+#         )
+#         cls.url = reverse('notes:detail', args=(cls.notes.slug,))
+#         cls.user = User.objects.create(username='Автор')
+#         cls.auth_client = Client()
+#         cls.auth_client.force_login(cls.user)
+#         cls.form_data = {'text': cls.NOTE_TEXT}
 
-    def test_anonymous_user_cant_create_comment(self):
-        self.client.post(self.url, data=self.form_data)
-        comments_count = Note.objects.count()
-        self.assertEqual(comments_count, 0)
+#     def test_anonymous_user_cant_create_comment(self):
+#         self.client.post(self.url, data=self.form_data)
+#         notes_count = Note.objects.count()
+#         self.assertEqual(notes_count, 0)
 
-    def test_user_can_create_comment(self):
-        response = self.auth_client.post(self.url, data=self.form_data)
-        self.assertRedirects(response, f'{self.url}#detail')
-        comments_count = Note.objects.count()
-        self.assertEqual(comments_count, 1)
-        comment = Note.objects.get()
-        self.assertEqual(comment.text, self.NOTE_TEXT)
-        self.assertEqual(comment.note, self.note)
-        self.assertEqual(comment.author, self.user)
+#     # def test_user_can_create_comment(self):
+#     #     response = self.auth_client.post(self.url, data=self.form_data)
+#     #     self.assertRedirects(response, f'{self.url}#add')
+#     #     notes_count = Note.objects.count()
+#     #     self.assertEqual(notes_count, 1)
+#     #     note = Note.objects.get()
+#     #     self.assertEqual(note.text, self.NOTE_TEXT)
+#     #     self.assertEqual(note.slug, self.notes)
+#     #     self.assertEqual(note.author, self.user)
+
+
+# # class TestCommentEditDelete(TestCase):
+# #     NOTE_TEXT = 'Текст комментария'
+# #     NEW_NOTE_TEXT = 'Обновлённый комментарий'
+
+# #     @classmethod
+# #     def setUpTestData(cls):
+# #         cls.notes = Note.objects.create(title='Заголовок', text='Текст', slug='Zogolovok')
+# #         # Формируем адрес блока с комментариями, который понадобится для тестов.
+# #         notes_url = reverse('notes:detail', args=(cls.notes.slug,))  # Адрес новости.
+# #         cls.url_to_notes = notes_url + '#id_title'  # Адрес блока с комментариями.
+# #         # Создаём пользователя - автора комментария.
+# #         cls.author = User.objects.create(username='Автор')
+# #         # Создаём клиент для пользователя-автора.
+# #         cls.author_client = Client()
+# #         # "Логиним" пользователя в клиенте.
+# #         cls.author_client.force_login(cls.author)
+# #         # Делаем всё то же самое для пользователя-читателя.
+# #         cls.reader = User.objects.create(username='Читатель')
+# #         cls.reader_client = Client()
+# #         cls.reader_client.force_login(cls.reader)
+# #         # Создаём объект комментария.
+# #         cls.addnotes = Note.objects.create(
+# #             news=cls.notes,
+# #             author=cls.author,
+# #             text=cls.NOTE_TEXT
+# #         )
+# #         # URL для редактирования комментария.
+# #         cls.edit_url = reverse('notes:edit', args=(cls.addnotes.slug,)) 
+# #         # URL для удаления комментария.
+# #         cls.delete_url = reverse('notes:delete', args=(cls.addnotes.slug,))  
+# #         # Формируем данные для POST-запроса по обновлению комментария.
+# #         cls.form_data = {'text': cls.NEW_NOTE_TEXT}
+
+# #     def test_author_can_delete_comment(self):
+# #         response = self.author_client.delete(self.delete_url)
+# #         self.assertRedirects(response, self.url_to_notes)
+# #         comments_count = Note.objects.count()
+# #         self.assertEqual(comments_count, 0)
