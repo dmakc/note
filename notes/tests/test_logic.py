851ddbb6467e588edb7ab10dad4b56b9516d5e78
Максим Давыдов <m.davidov92@gmail.com>
2023-11-27 00:17:57 +0300

    Add code test logic

--- notes/tests/test_logic.py
+++ notes/tests/test_logic.py
@@ -1,82 +1,80 @@
-# from http import HTTPStatus
+from http import HTTPStatus
 
-# from django.contrib.auth import get_user_model
-# from django.test import Client, TestCase
-# from django.urls import reverse
+from django.contrib.auth import get_user_model
+from django.test import Client, TestCase
+from django.urls import reverse
 
-# from notes.forms import WARNING
-# from notes.models import Note
+from notes.forms import WARNING
+from notes.models import Note
 
-# User = get_user_model()
-
-
-# class TestNoteCreation(TestCase):
-#     NOTE_TEXT = 'Текст комментария'
-
-#     @classmethod
-#     def setUpTestData(cls):
-#         cls.notes = Note.objects.create(
-#             title='Заголовок',
-#             text='Текст',
-#             slug='not-slug',
-#         )
-#         cls.url = reverse('notes:detail', args=(cls.notes.slug,))
-#         cls.user = User.objects.create(username='Автор')
-#         cls.auth_client = Client()
-#         cls.auth_client.force_login(cls.user)
-#         cls.form_data = {'text': cls.NOTE_TEXT}
-
-#     def test_anonymous_user_cant_create_comment(self):
-#         self.client.post(self.url, data=self.form_data)
-#         notes_count = Note.objects.count()
-#         self.assertEqual(notes_count, 0)
-
-#     # def test_user_can_create_comment(self):
-#     #     response = self.auth_client.post(self.url, data=self.form_data)
-#     #     self.assertRedirects(response, f'{self.url}#add')
-#     #     notes_count = Note.objects.count()
-#     #     self.assertEqual(notes_count, 1)
-#     #     note = Note.objects.get()
-#     #     self.assertEqual(note.text, self.NOTE_TEXT)
-#     #     self.assertEqual(note.slug, self.notes)
-#     #     self.assertEqual(note.author, self.user)
+User = get_user_model()
 
 
-# # class TestCommentEditDelete(TestCase):
-# #     NOTE_TEXT = 'Текст комментария'
-# #     NEW_NOTE_TEXT = 'Обновлённый комментарий'
+class TestNoteCreation(TestCase):
+    NOTE_TEXT = 'Заметка'
 
-# #     @classmethod
-# #     def setUpTestData(cls):
-# #         cls.notes = Note.objects.create(title='Заголовок', text='Текст', slug='Zogolovok')
-# #         # Формируем адрес блока с комментариями, который понадобится для тестов.
-# #         notes_url = reverse('notes:detail', args=(cls.notes.slug,))  # Адрес новости.
-# #         cls.url_to_notes = notes_url + '#id_title'  # Адрес блока с комментариями.
-# #         # Создаём пользователя - автора комментария.
-# #         cls.author = User.objects.create(username='Автор')
-# #         # Создаём клиент для пользователя-автора.
-# #         cls.author_client = Client()
-# #         # "Логиним" пользователя в клиенте.
-# #         cls.author_client.force_login(cls.author)
-# #         # Делаем всё то же самое для пользователя-читателя.
-# #         cls.reader = User.objects.create(username='Читатель')
-# #         cls.reader_client = Client()
-# #         cls.reader_client.force_login(cls.reader)
-# #         # Создаём объект комментария.
-# #         cls.addnotes = Note.objects.create(
-# #             news=cls.notes,
-# #             author=cls.author,
-# #             text=cls.NOTE_TEXT
-# #         )
-# #         # URL для редактирования комментария.
-# #         cls.edit_url = reverse('notes:edit', args=(cls.addnotes.slug,)) 
-# #         # URL для удаления комментария.
-# #         cls.delete_url = reverse('notes:delete', args=(cls.addnotes.slug,))  
-# #         # Формируем данные для POST-запроса по обновлению комментария.
-# #         cls.form_data = {'text': cls.NEW_NOTE_TEXT}
+    @classmethod
+    def setUpTestData(cls):
+        cls.author = User.objects.create(username='Автор')
+        cls.author_client = Client()
+        cls.author_client.force_login(cls.author)
+        cls.notes = Note.objects.create(
+            title='Заметка',
+            text='Текст заметки',
+            slug='not-slug',
+            author=cls.author
+        )
+        cls.url = reverse('notes:add')
+        cls.form_data = {
+            'title': cls.NOTE_TEXT,
+            'text': 'Текст заметки',
+            'slug': 'not-slug',
+        }
 
-# #     def test_author_can_delete_comment(self):
-# #         response = self.author_client.delete(self.delete_url)
-# #         self.assertRedirects(response, self.url_to_notes)
-# #         comments_count = Note.objects.count()
-# #         self.assertEqual(comments_count, 0)
+    def test_anonymous_user_cant_create_comment(self):
+        self.client.post(self.url, data=self.form_data)
+        comments_count = Note.objects.count()
+        self.assertEqual(comments_count, 1)
+
+    def test_user_can_create_comment(self):
+        response = self.author_client.post(self.url, data=self.form_data)
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+        note_count = Note.objects.count()
+        self.assertEqual(note_count, 1)
+        note = Note.objects.get()
+        self.assertEqual(note.title, self.NOTE_TEXT)
+
+    def test_not_unique_slug(self):
+        bad_words_data = {'slug': f'{self.notes.slug}'}
+        response = self.author_client.post(self.url, data=bad_words_data)
+        self.assertFormError(
+            response,
+            form='form',
+            field='slug',
+            errors=(self.notes.slug + WARNING),
+        )
+        note_count = Note.objects.count()
+        self.assertEqual(note_count, 1)
+
+
+class TestNoteCreation1(TestCase):
+    NOTE_TEXT = 'Заметка'
+
+    @classmethod
+    def setUpTestData(cls):
+        cls.author = User.objects.create(username='Автор')
+        cls.author_client = Client()
+        cls.author_client.force_login(cls.author)
+        cls.notes = Note.objects.create(
+            title='Заметка',
+            text='Текст заметки',
+            author=cls.author
+        )
+        cls.url = reverse('notes:add')
+        cls.form_data = {
+            'title': cls.NOTE_TEXT,
+            'text': 'Текст заметки',
+        }
+
+    def test_empty_slug(self):
+        pass
